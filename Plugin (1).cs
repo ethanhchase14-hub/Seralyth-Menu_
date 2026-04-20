daa099d36063fbdf46fbb948359afa1b16413d19
ethanhchase14-hub <ethan.h.chase14@gmail.com>
2026-04-20 14:32:40 -0500

    Add files via upload

---
+++ Plugin (1).cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+
+namespace Console
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // Put this snippet of code in your BaseUnityPlugin
+        void Start() => Console.LoadConsole();
+    }
+}
